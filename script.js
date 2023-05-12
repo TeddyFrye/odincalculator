ae6240a5e7335316ac481157ed193dbcd288f44f
TeddyFrye <tfrye1227@gmail.com>
2023-05-12 12:37:28 -0500

    Fixes odin bugs, redoes script

--- script.js
+++ script.js
@@ -1,101 +1,78 @@
-//Simple math items
-const add = function(a,b) {
-	return a+b;
-};
-
-const subtract = function(a,b) {
-	return a-b
-};
-
-const sum = function(myArray) {
-    let total = 0;
-    for (let i = 0; i < myArray.length; i++) {
-      total += myArray[i];
-    }
-    return total;
-};
-
-function multiply(myArray) {
-  let total = 1;
-  for (let i = 0; i < myArray.length; i++) {
-    total *= myArray[i];
-  }
-  return total;
-}
-
-function power(a,b) {
-  return a**b;
-}
-
-const factorial = function(myArray) {
-    let result = 1;
-    for (let i = 1; i <= myArray; i++) {
-      result *= i;
-    }
-    return result;
-};
-
-//Creating the operate function
-function operate(num1, operator,num2){
-    switch (operator){
-        case "+":
-            return add(num1,num2);
-        case "-":
-            return subtract(num1,num2);
-        case "*":
-            return multiply(num1,num2);
-        case "/":
-            return divide(num1,num2);
-        case "**":
-            return power(num1,num2);
-        case "!":
-            return factorial(num1,num2);
-    }
-}
-
-//Making buttons do stuff
 const display = document.getElementById("display");
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator");
-const clearButton = document.querySelector(".clear");
-const equalsButton = document.querySelector(".equals");
+const buttons = document.querySelectorAll("button");
+let num1 = "";
+let num2 = "";
+let operator = "";
 
-let num1 = null;
-let num2 = null;
-let operator = null;
-
-numbers.forEach((number) => {
-  number.addEventListener("click", () => {
-    display.textContent += number.textContent;
-  });
+// Add click event listeners to all buttons
+buttons.forEach(button => {
+  button.addEventListener("click", handleClick);
 });
 
-operators.forEach((operatorButton) => {
-  operatorButton.addEventListener("click", () => {
-    if (num1 === null) {
-      num1 = Number(display.textContent);
-      operator = operatorButton.dataset.operator;
-      display.textContent = num1;
-    } else {
-      num2 = Number(display.textContent);
-      const result = operate(operator, num1, num2);
-      display.textContent = result;
-      num1 = result;
-      operator = operatorButton.dataset.operator;
-      num2 = null;
-    }
-  });
-});
+// Handle button click
+function handleClick(event) {
+  const value = event.target.textContent;
+  
+  if (value >= "0" && value <= "9") {
+    handleNumber(value);
+  } else if (value === "+" || value === "-" || value === "*" || value === "/") {
+    handleOperator(value);
+  } else if (value === "=") {
+    handleEquals();
+  } else if (value === "C") {
+    handleClear();
+  }
+}
 
-equalsButton.addEventListener("click", () => {
-    num2 = parseFloat(display.textContent);
-    display.textContent = operate(operator,num1,num2);
-});
+// Handle number button click
+function handleNumber(value) {
+  if (operator === "") {
+    num1 += value;
+    display.textContent = num1;
+  } else {
+    num2 += value;
+    display.textContent = num2;
+  }
+}
 
+// Handle operator button click
+function handleOperator(value) {
+  operator = value;
+}
 
-clearButton.addEventListener("click", () => {
+// Handle equals button click
+function handleEquals() {
+  const num1 = parseFloat(num1);
+  const num2 = parseFloat(num2);
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      result = "";
+  }
+
+  display.textContent = result;
+  num1 = result.toString();
+  num2 = "";
+  operator = "";
+}
+
+// Handle clear button click
+function handleClear() {
+  num1 = "";
+  num2 = "";
+  operator = "";
   display.textContent = "";
-  num1 = null;
-  num2 = null;
-  operator = null;
-});
+}
